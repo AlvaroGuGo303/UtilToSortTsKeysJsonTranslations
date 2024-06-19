a87963309ceb73c4573f82a1024724c65d813674
AlvaroGuillen <alvaro.guillen@aeinnova.com>
2024-06-19 12:22:38 +0200

    fix script filename

--- 1to2_convertJsToJson.js
+++ 1to2_convertJsToJson.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 const inputFilePath = "./1_Input_Ts_to_Js/FILENAME.js";  // Put here the name of your file
-const outputFilePath = "./1_Input_Ts_to_Js/jpSgi.json";  // Put here the name of your file
+const outputFilePath = "./1_Input_Ts_to_Js/FILENAME.json";  // Put here the name of your file
 
 const { locale } = require(inputFilePath);
 // Write the JSON file
